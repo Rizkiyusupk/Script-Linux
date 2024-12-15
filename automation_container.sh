04b3a0b703fe5d04237970882cbe2db19dd08d67
Rizkiyusupk <trumpargoy@gmail.com>
2024-12-15 21:06:20 +0700

    Update automation_container.sh

--- automation_container.sh
+++ automation_container.sh
@@ -46,7 +46,7 @@ then
                         while [ $counter -le $bos_c ];
                         do
                                 echo "hello world"
-                         docker run -it --name mariadb_container"$counter" mariadb:latest 
+                         docker run -d --name mariadb1"$counter" -e MARIADB_ROOT_PASSWORD=iki123 mariadb:latest
                                ((counter++))
                         done
 
