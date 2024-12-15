e704530cf20f72d3eca060615379471efd1ff23d
Rizkiyusupk <trumpargoy@gmail.com>
2024-12-15 20:52:55 +0700

    Add files via upload

---
+++ automation_container.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+echo "haii boss butuh container buat deploy??"
+read bos
+lower_bos=$(echo "$bos" | tr "[:upper:]" "[:lower:]")
+if [ $lower_bos == "iya" ];
+then
+
+        echo "baik mau ngambil image yang mana??"
+        images=(ubuntu mysql maridb)
+        for i in {!}
+        do
+                for j in "${images[@]}"
+                do
+                        echo "$i)$j"
+                done
+        done
+        read bos2
+        lower_bos=$(echo "$bos2" | tr "[:upper:]" "[:lower:]")
+        if [ $lower_bos == 'ubuntu' ];
+	then
+		 echo "baik bos butuh berapa container??"
+                        read bos_c
+                        counter=1
+                        while [ $counter -le $bos_c ];
+                        do
+                                echo "hello world"
+                        docker run -it --name ubuntu_"$counter" ubuntu:latest bash
+                               ((counter++))
+                        done
+		elif [ $lower_bos == 'mysql' ]
+		then
+			echo "baik bos butuh berapa container??"
+                        read bos_c
+                        counter=1
+                        while [ $counter -le $bos_c ];
+                        do
+                                echo "hello world"
+                         docker run -it --name sql_container'$counter' -e  mysql_root_password=iki123  mysql:latest 
+                               ((counter++))
+                        done
+		elif [ $lower_bos == 'mariadb' ]
+		then
+			  echo "baik bos butuh berapa container??"
+                        read bos_c
+                        counter=1
+                        while [ $counter -le $bos_c ];
+                        do
+                                echo "hello world"
+                         docker run -it --name mariadb_container"$counter" mariadb:latest 
+                               ((counter++))
+                        done
+
+	fi
+fi
