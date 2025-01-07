23aa2c45fffeedae64bb18275f7250c95885c097
Rizkiyusupk <trumpargoy@gmail.com>
2025-01-07 10:27:23 +0700

    Update automation_container.sh

--- automation_container.sh
+++ automation_container.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "haii boss butuh container buat deploy??"
+echo "haii boss want to make a container,y/n??"
 read bos
 lower_bos=$(echo "$bos" | tr "[:upper:]" "[:lower:]")
-if [ $lower_bos == "iya" ];
+if [ $lower_bos == "y" ];
 then
 
-        echo "baik mau ngambil image yang mana??"
+        echo "what images you want??"
         images=(ubuntu mysql maridb)
         for i in {!}
         do
@@ -18,35 +18,46 @@ then
         lower_bos=$(echo "$bos2" | tr "[:upper:]" "[:lower:]")
         if [ $lower_bos == 'ubuntu' ];
 	then
-		 echo "baik bos butuh berapa container??"
+		 echo "how many container you need??"
                         read bos_c
                         counter=1
                         while [ $counter -le $bos_c ];
                         do
-                                echo "hello world"
+                                
                         docker run -it --name ubuntu_"$counter" ubuntu:latest bash
                                ((counter++))
                         done
 		elif [ $lower_bos == 'mysql' ]
 		then
-			echo "baik bos butuh berapa container??"
+			echo "how many container you need??"
                         read bos_c
+			echo "because this is a database container so you need a root password"
+   			echo "input for root password"
+      			read password
+	 		echo "name for your container"
+    			read cont_name
+       			lower_cont_name=$(echo "$cont_name" | tr "[:upper:]" "[:lower:]")
+	 		lower_password=$(echo "$password" | tr "[::upper]" "[:lower:]")
                         counter=1
                         while [ $counter -le $bos_c ];
                         do
-                                echo "hello world"
-                         docker run -it --name sql_container'$counter' -e  MYSQL_ROOT_PASSWORD=your_secret_password  mysql:latest
+                         docker run -d --name "$lower_cont_name"-"$counter" -e  MYSQL_ROOT_PASSWORD=$lower_password mysql:latest
                                ((counter++))
                         done
 		elif [ $lower_bos == 'mariadb' ]
 		then
-			  echo "baik bos butuh berapa container??"
+			echo "how many container you need??"
                         read bos_c
-                        counter=1
+			echo "because this is a database container so you need a root password"
+   			read password
+      			lower_password=$(echo "$password" | tr "[:upper:]" "[:lower:]")
+	 		echo "name for your container"
+    			read cont_name
+       			lower_cont_name=$(echo "$cont_name" | tr "[:upper:]" "[:lower:]")
+   			counter=1
                         while [ $counter -le $bos_c ];
                         do
-                                echo "hello world"
-                         docker run -d --name mariadb1"$counter" -e MARIADB_ROOT_PASSWORD=your_secret_password mariadb:latest
+                         docker run -d --name "$lower_password"-"$counter" -e MARIADB_ROOT_PASSWORD=your_secret_password mariadb:latest
                                ((counter++))
                         done
 
