142d64a3f1a69719ba2b95c195942cab495c175f
Rizkiyusupk <trumpargoy@gmail.com>
2025-05-05 22:51:12 +0700

    Update auto_image_push.sh
    fix file

--- auto_image_push.sh
+++ auto_image_push.sh
@@ -3,9 +3,9 @@ do
 	echo "masukan nama image mu untuk di tag:"
 	read image
 docker tag $image rizki736/$image
+docker push rizki736/$image
         echo "apakah masih mau buat tag lagi:y/n"
 	read user
-docker push rizki736/$image
 	if [ $user == 'y' ]
 	then
 		echo "baikk masukan lagi nama imagenya"
