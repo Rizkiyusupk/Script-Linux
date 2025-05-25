ae546e3d91ce07e2b997ffb84604a8e1335cc2f3
Rizkiyusupk <trumpargoy@gmail.com>
2025-05-25 22:28:18 +0700

    Add files via upload

---
+++ automation_cek_with_smtp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+while true 
+do
+	echo "system cek otomatis dari server"
+	echo "sedang mengecek....."
+	sleep 5
+	cek=$(systemctl status nginx | grep -oP 'status=\K[0-9]+' |  head -n1)
+	if [[ $cek -eq 0 ]];
+	then
+		echo "sistem mu baik baik saja silahkan kembali beraktivitas!!!"
+	else
+		echo "ada eror di nginx webserver mu!!!"
+                echo "mengirim ke alamat emaill!!"
+                echo "mengirim..."
+                sleep 10
+                echo "EROR TERDETEKSI!!! SEGERA TANGANI JIKA TIDAK WEBSITE AKAN DOWN UNTUK WAKTU YANG LAMA" | mail -s "EROR TERDETEKSI" rizkiyk61@gmail.com
+		break
+
+	fi
+done
