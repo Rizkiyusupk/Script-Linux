38c6be4a3984a7cc3e1a1f76fa8109a2ec073bc0
rizkiyusupk <trumpargoy@gmail.com>
2026-05-18 19:43:41 +0700

    Delete automation_add_user_using_csv.sh

--- automation_add_user_using_csv.sh
+++
@@ -1,9 +0,0 @@
-while IFS=',' read -r username password
-do
-    if [ "$username" != "username" ]; then
-        useradd -m "$username"
-        echo "$username:$password" | chpasswd
-        echo "User '$username' dibuat dengan password."
-    fi
-done < 'user.csv'
-
